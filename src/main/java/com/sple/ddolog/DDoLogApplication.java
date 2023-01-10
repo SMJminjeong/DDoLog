da2338bc282ab3c965b661b1caf399f6238398a2
SMJminjeong <sdwp0920@gmail.com>
2023-01-10 23:07:02 +0900

    Initial commit

---
+++ src/main/java/com/sple/ddolog/DDoLogApplication.java
@@ -0,0 +1,13 @@
+package com.sple.ddolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DDoLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DDoLogApplication.class, args);
+    }
+
+}
